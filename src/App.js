a3c94cd43f6303a2cdf6d887b6e25ea979f97b29
Rahul N <rahul.eee32@gmail.com>
2022-04-23 13:26:02 +0530

    Created Header component, added dummy data and styling for the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import Header from "./Header"
+import "./Header.css"
+
 function App() {
   return (
     <div>
-      <div className="Header">Phone Directory</div>
-      <button>Add</button>
-      <div>
-        <span>Name</span><br/>
-        <span>Phone</span>
+      <Header/>
+      <button className="add-btn">Add</button>
+      <div className="grid-container">
+        <span className="grid-item name-heading">Name</span><br/>
+        <span className="grid-item phone-heading">Phone</span>
+      </div>
+      <div className="grid-container">
+        <span className="grid-item">Tom</span><br/>
+        <span className="grid-item">1231231321</span>
+      </div>
+      <div className="grid-container">
+        <span className="grid-item">Jerry</span><br/>
+        <span className="grid-item">1231231321</span>
       </div>
     </div>
   );
