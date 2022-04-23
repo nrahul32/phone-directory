45e00cb066df042c2d035f11ebfc47198ded9110
Rahul N <rahul.eee32@gmail.com>
2022-04-23 21:55:19 +0530

    Used state to store the name, phone entered and displayed them in the div

--- src/AddContact.js
+++ src/AddContact.js
@@ -3,7 +3,23 @@ import Header from "./Header";
 import "./AddContact.css"
 
 class AddContact extends Component {
+    constructor(){
+        super();
+        this.state = {
+            id: 0,
+            name: '',
+            phone: ''
+        }
+    }
+
+    inputChangeHandler = (e) => {
+        const state = this.state;
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    }
+
     render() {
+        const {name, phone} = this.state;
         return (
             <div>
                 <Header heading="Add Contact" />
@@ -12,13 +28,13 @@ class AddContact extends Component {
 
                 <form>
                     <label htmlFor="name" className="label-control">Name:</label><br/>
-                    <input id="name" className="input-control"></input><br/>
+                    <input id="name" className="input-control" name="name" onChange={this.inputChangeHandler}></input><br/>
                     <label htmlFor="name" className="label-control">Phone:</label><br/>
-                    <input id="name" className="input-control"></input><br/>
+                    <input id="name" className="input-control" name="phone" onChange={this.inputChangeHandler}></input><br/>
                     <div className="contact-info-container">
                         <span className="contact-info">Contact to be added</span><br/>
-                        <span className="contact-info">Name:</span><br/>
-                        <span className="contact-info">Phone:</span><br/>
+                        <span className="contact-info">Name: {name}</span><br/>
+                        <span className="contact-info">Phone: {phone}</span><br/>
                     </div>
                     <button className="btn add-btn">Add</button>
                 </form>
